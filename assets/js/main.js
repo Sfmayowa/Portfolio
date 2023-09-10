300adf75cca37f270803c164d995e4f93af8eaed
Mayowa Yusuf <sfmayowa@gmail.com>
2023-09-10 21:06:10 +0100

    Added Dark Mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-/*=============== SHOW MENU ===============*/
+// SHOW MENU
 const navMenu = document.getElementById("nav-menu"),
   navToggle = document.getElementById("nav-toggle");
 navClose = document.getElementById("nav-close");
@@ -17,7 +17,9 @@ if (navClose) {
   });
 }
 
-/*=============== REMOVE MENU MOBILE ===============*/
+
+
+// REMOVE MENU MOBILE
 const navLink = document.querySelectorAll(".nav__link");
 
 const linkAction = () => {
@@ -27,7 +29,9 @@ const linkAction = () => {
 };
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
-/*=============== SHADOW HEADER ===============*/
+
+
+// HEADER SHADOW
 const shadowHeader = () => {
   const header = document.getElementById("header");
 
@@ -37,7 +41,9 @@ const shadowHeader = () => {
 };
 window.addEventListener("scroll", shadowHeader);
 
-/*=============== EMAIL JS ===============*/
+
+
+// EMAIL JS
 const contactForm = document.getElementById('contact-form'),
       contactMessage = document.getElementById('contact-message')
 
@@ -66,10 +72,36 @@ const sendEmail = (e) =>{
 }
 
 contactForm.addEventListener('submit', sendEmail)
-/*=============== SHOW SCROLL UP ===============*/
 
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+// LIGHT DARK THEME
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
 
-/*=============== DARK LIGHT THEME ===============*/
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
